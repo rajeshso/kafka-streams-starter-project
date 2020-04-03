6a0b1e2b878b21fc6315129c936d8155f770b030
Rajesh <fun_rajesh@yahoo.com>
2020-04-03 20:34:49 +0100

    Word Count - second attempt

--- word-count/src/main/java/com/n2/raj/kafka/streams/WordCountApp.java
+++ word-count/src/main/java/com/n2/raj/kafka/streams/WordCountApp.java
@@ -10,49 +10,61 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.ValueMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WordCountApp {
   private static final Logger LOGGER = LoggerFactory.getLogger(WordCountApp.class);
   public static void main(String[] args) {
-    Properties properties = createProperties();
+    final WordCountApp wordCountApp = new WordCountApp();
+    final Properties config = wordCountApp.createProperties();
+    final Topology topology = wordCountApp.createTopology();
 
-    final Topology topology = createTopology();
-
-    KafkaStreams kafkaStreams = new KafkaStreams(topology, properties);
-    kafkaStreams.start();
-    LOGGER.info(kafkaStreams.toString());
-
+    KafkaStreams kafkaStreams = new KafkaStreams(topology, config);
     // Shutdown hook
     Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+    kafkaStreams.start();
+    LOGGER.info(kafkaStreams.toString());
+    // Update:
+    // print the topology every 10 seconds for learning purposes
+    while(true){
+      kafkaStreams.localThreadsMetadata().forEach(data -> System.out.println(data));
+      try {
+        Thread.sleep(5000);
+      } catch (InterruptedException e) {
+        break;
+      }
+    }
   }
 
-  private static Topology createTopology() {
+  public Topology createTopology(){
     StreamsBuilder builder = new StreamsBuilder();
-    //1. Stream from Kafka
-    final KStream<String, String> textLines = builder.stream("word-count-input");
-    //2. Map Values to lowercase
-    final KTable<String, Long> wordCounts = textLines.mapValues(
-        (ValueMapper<String, String>) String::toLowerCase)
-                  //3. flatmap values split by space
-                  .flatMapValues(lowerCasedTextLine -> Arrays.asList(lowerCasedTextLine.split("")))
-                  //4. select key to apply key (we discard the old key)
-                  .selectKey((ignoredKey, word) -> word)
-                  //5. Group by key before aggregation
-                  .groupByKey()
-                  //6. Count occurences
-                  .count(Named.as("Counts"));
-    //7. To in order to write the value back to Kafka
+    // 1 - stream from Kafka
+
+    KStream<String, String> textLines = builder.stream("word-count-input");
+    KTable<String, Long> wordCounts = textLines
+        // 2 - map values to lowercase
+        .mapValues(textLine -> textLine.toLowerCase())
+        // can be alternatively written as:
+        // .mapValues(String::toLowerCase)
+        // 3 - flatmap values split by space
+        .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+        // 4 - select key to apply a key (we discard the old key)
+        .selectKey((key, word) -> word)
+        // 5 - group by key before aggregation
+        .groupByKey()
+        // 6 - count occurences
+        .count(Materialized.as("Counts"));
+
+    // 7 - to in order to write the results back to kafka
     wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
 
     return builder.build();
   }
 
-  private static Properties createProperties() {
+  private Properties createProperties() {
     Properties config = new Properties(6);
     config.put(StreamsConfig.APPLICATION_ID_CONFIG,"streams-starter-app");
     config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
